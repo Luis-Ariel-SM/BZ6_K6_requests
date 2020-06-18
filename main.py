3661449fb910053034d1ea78792b2acdcaf453dc
Luis Ariel Santander Méndez <luisarielsantander@gmail.com>
2020-06-18 23:42:08 +0200

    Busca API en controller

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import requests
+from tkinter import *
+from tkinter import ttk
 
-URL = 'http://www.omdbapi.com/?s={}&apikey=facf808d'
+import films
 
-peli = input ('Buscar: ')
+class MainApp(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.title('Busca peliculas')
+        searcher = films.Controller(self)
+        searcher.pack()
 
-respuesta = requests.get(URL.format(peli)) # Se puede meter la url directamente en el parentesis igual
+    def main (self):
+        self.mainloop()
 
-# print (respuesta.text)
-
-mijson = respuesta.json() # Para cambiar una cadena a Json la diferencia se nota en las comillas sencillas porque asi es como json 
-# interpreta las cadenas. Ademas con el debug se ve como el json es un diccionario
-
-print (mijson.get ('Search')[0].get('Title'))
-print (mijson.get ('Search')[0].get('Poster'))
+if __name__ == '__main__':
+    app = MainApp()
+    app.main()
